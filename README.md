40fb238565c2aeb7bcfca37aa8cdbec87e2f1b28
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-03-14 22:40:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# nodengine
+
+![Last version](https://img.shields.io/github/tag/Kikobeats/nodengine.svg?style=flat-square)
+[![Build Status](http://img.shields.io/travis/Kikobeats/nodengine/master.svg?style=flat-square)](https://travis-ci.org/Kikobeats/nodengine)
+[![Dependency status](http://img.shields.io/david/Kikobeats/nodengine.svg?style=flat-square)](https://david-dm.org/Kikobeats/nodengine)
+[![Dev Dependencies Status](http://img.shields.io/david/dev/Kikobeats/nodengine.svg?style=flat-square)](https://david-dm.org/Kikobeats/nodengine#info=devDependencies)
+[![NPM Status](http://img.shields.io/npm/dm/nodengine.svg?style=flat-square)](https://www.npmjs.org/package/nodengine)
+[![Donate](https://img.shields.io/badge/donate-paypal-blue.svg?style=flat-square)](https://paypal.me/Kikobeats)
+
+> NodeJS switcher based on package.json.
+
+It needs a global version manager as [n](https://www.npmjs.com/package/n) or [nvm](https://www.npmjs.com/package/nvm).
+
+## Install
+
+```bash
+$ npm install nodengine --global
+```
+
+## Usage
+
+Just run `nodengine` to change to current package.json version. Just it!
+
+## License
+
+MIT © [Kiko Beats](http://kikobeats.com)
---
+++ bin/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+'use strict'
+
+var _pkg = require('../package.json')
+require('update-notifier')({pkg: _pkg}).notify()
+require('meow')(_pkg)
+
+var path = require('path')
+var which = require('which')
+var semver = require('semver')
+var eachAsync = require('each-async')
+var spawn = require('child_process').spawn
+var nodeVersions = require('node-versions')
+
+function processExit (code) {
+  return process.exit(code || 0)
+}
+
+function nodeSwitcher (nodeVersion) {
+  var binaries = {
+    n: ['use', nodeVersion]
+  }
+
+  function swicher (binary) {
+    return spawn(binary, binaries[binary], {stdio: 'inherit'})
+  }
+
+  swicher.binaries = Object.keys(binaries)
+  return swicher
+}
+
+var pkg
+
+try {
+  pkg = require(path.resolve('package.json'))
+} catch (err) {
+  processExit()
+}
+
+var nodeVersion = pkg.engines && pkg.engines.node
+if (!nodeVersion) processExit()
+
+var maxNodeVersion = semver.maxSatisfying(nodeVersions, nodeVersion)
+var switcher = nodeSwitcher(maxNodeVersion)
+var binaries = []
+
+eachAsync(switcher.binaries, function (binary, index, next) {
+  which(binary, function (err) {
+    if (!err) binaries.push(binary)
+    return next()
+  })
+}, function () {
+  if (binaries.length) return switcher(binaries.shift())
+})
