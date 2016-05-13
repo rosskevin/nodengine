e23cbd90c8108aeba3d810a92618909ce0c543bc
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-05-14 00:03:43 +0200

    Fetch node versions

--- bin/index.js
+++ bin/index.js
@@ -3,19 +3,16 @@
 'use strict'
 
 var _pkg = require('../package.json')
-require('update-notifier')({pkg: _pkg}).notify()
 require('meow')(_pkg)
+var config = require('./config')
 
 var path = require('path')
 var async = require('async')
 var semver = require('semver')
 var which = require('./which')
 var Switcher = require('./switcher')
-var nodeVersions = require('node-versions')
 
-function processExit (code) {
-  return process.exit(code || 0)
-}
+var processExit = require('./util').processExit
 
 var pkg
 
@@ -28,22 +25,25 @@ try {
 var nodeVersion = pkg.engines && pkg.engines.node
 if (!nodeVersion) processExit()
 
-var maxNodeVersion = semver.maxSatisfying(nodeVersions, nodeVersion)
-var switcher = Switcher(maxNodeVersion)
-var switcherBin = null
+config(function (nodeVersions) {
+  var maxNodeVersion = semver.maxSatisfying(nodeVersions, nodeVersion)
 
-function getBin (next) {
-  var bin = switcher.binaries.pop()
-  which(bin, function (err) {
-    if (!err) switcherBin = bin
-    return next()
+  var switcher = Switcher(maxNodeVersion)
+  var switcherBin = null
+
+  function getBin (next) {
+    var bin = switcher.binaries.pop()
+    which(bin, function (err) {
+      if (!err) switcherBin = bin
+      return next()
+    })
+  }
+
+  function whileCondition () {
+    return switcherBin == null && switcher.binaries.length !== 0
+  }
+
+  async.doWhilst(getBin, whileCondition, function () {
+    if (switcherBin) return switcher(switcherBin)
   })
-}
-
-function whileCondition () {
-  return switcherBin == null && switcher.binaries.length !== 0
-}
-
-async.doWhilst(getBin, whileCondition, function () {
-  if (switcherBin) return switcher(switcherBin)
 })
