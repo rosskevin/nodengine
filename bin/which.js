7eb4e190d6dc9aa0544fe2903e478171f8b53258
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-05-13 10:12:55 +0200

    Remove process.nexttick

--- bin/which.js
+++ bin/which.js
@@ -6,10 +6,7 @@ var NVM_DIR = process.env.NVM_DIR
 function which (bin, cb) {
   var binaries = {
     n: function () { return _which(bin, cb) },
-    nvm: function () {
-      if (NVM_DIR) return process.nextTick(cb)
-      process.nextTick(function () { return cb('nope') })
-    }
+    nvm: function () { return cb(NVM_DIR ? null : 'nope') }
   }
 
   return binaries[bin]()
